418c94b200ac9def266e4e8a0f1b8ab23616f4a4
Alex <stark.11raj@gmail.com>
2024-03-24 03:02:13 +0000

    Refactoring

--- TelegramChannelManager.Server/Commands/ExecutedCommandParapms.cs
+++ TelegramChannelManager.Server/Commands/ExecutedCommandParapms.cs
@@ -8,21 +8,17 @@ namespace TelegramChannelManager.Server.Commands
 
         public ExecutedCommandParapms(Message sentMessage) : this(sentMessage, null)
         {
-
+            SentMessage = sentMessage;
         }
 
-        public ExecutedCommandParapms(UserState userState) : this(null, userState)
+        public ExecutedCommandParapms(Message sentMessage, UserState? userState)
         {
-
+            SentMessage = sentMessage;
+            UserState = userState;
         }
 
-        public ExecutedCommandParapms(Message? message, UserState? userState)
-        {
+        public Message SentMessage { get; }
 
-        }
-
-        public Message? SentMessage { get; set; }
-
-        public UserState? UserState { get; set; }
+        public UserState? UserState { get; }
     }
 }
