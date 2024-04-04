74034342e23d218fbab82b17fdbca4d2ea9d37fb
Alex <stark.11raj@gmail.com>
2024-04-04 03:19:37 +0100

    delete old project

--- TelegramChannelManager.Server/Services/IUserContextManager.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ChannelManager.API.Services
-{
-    public interface IUserContextManager
-    {
-        UserContext CreateNewUserContext(long chatId);
-        UserContext? GetUserContext(long chatId);
-        bool TryGetUserContext(long chatId, out UserContext? userContext);
-    }
-}
