bca3d6f30b9de7869d43710367ea30044b188db5
Alex <stark.11raj@gmail.com>
2024-04-04 03:16:25 +0100

    Add migration

---
+++ ChannelManager.API/Migrations/20240404021523_NewMigration.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ChannelManager.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class NewMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "BotToken",
+                table: "User",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<int>(
+                name: "State",
+                table: "User",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "BotToken",
+                table: "User");
+
+            migrationBuilder.DropColumn(
+                name: "State",
+                table: "User");
+        }
+    }
+}
