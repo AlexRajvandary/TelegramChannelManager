13b219612a4de926d383cb73ebcc1396a6be0056
Alex <stark.11raj@gmail.com>
2024-04-16 20:52:53 +0100

    Modified services

---
+++ ChannelManager.API/Services/ITelegramClientsManager.cs
@@ -0,0 +1,11 @@
+﻿using Telegram.Bot;
+
+namespace ChannelManager.API.Services
+{
+    public interface ITelegramClientsManager
+    {
+        Task<ITelegramBotClient> CreateNewBotClientAsync(Guid userId, string botToken, CancellationToken cancellationToken);
+
+        ITelegramBotClient? GetBotClient(Guid userId);
+    }
+}
---
+++ ChannelManager.API/Services/TelegramClientsManager.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Extensions.Options;
+using System.Collections.Concurrent;
+using Telegram.Bot;
+
+namespace ChannelManager.API.Services
+{
+    public class TelegramClientsManager: ITelegramClientsManager
+    {
+        private ConcurrentDictionary<Guid, ITelegramBotClient> telegramBotClients;
+        private BotConfiguration _botConfig;
+        private ILogger _logger;
+
+        public TelegramClientsManager(IOptions<BotConfiguration> botOptions, ILogger logger)
+        {
+            telegramBotClients = new ConcurrentDictionary<Guid, ITelegramBotClient>();
+            _botConfig = botOptions.Value;
+            _logger = logger;
+        }
+
+        public async Task<ITelegramBotClient> CreateNewBotClientAsync(Guid userId, string botToken, CancellationToken cancellationToken)
+        {
+            if (telegramBotClients.TryGetValue(userId, out ITelegramBotClient? value))
+            {
+                return value;
+            }
+            else
+            {
+                var userBotClient = new TelegramBotClient(botToken);
+                var webhookAddress = $"{_botConfig.HostAddress}/customerBot";
+                _logger.LogInformation("Setting webhook: {WebhookAddress}", webhookAddress);
+                await userBotClient.SetWebhookAsync(
+                    url: webhookAddress,
+                    cancellationToken: cancellationToken);
+
+                telegramBotClients.TryAdd(userId, new TelegramBotClient(botToken));
+                return userBotClient;
+            }
+        }
+
+        public ITelegramBotClient? GetBotClient(Guid userId)
+        {
+            telegramBotClients.TryGetValue(userId, out ITelegramBotClient? value);
+            return value;
+        }
+    }
+}
